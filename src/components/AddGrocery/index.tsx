dc1d68ef9d2ef8ef4022c231346bb73c7a726e5f
Bradley Burgess <bradleyburgess@gmail.com>
2020-11-30 23:54:56 -0500

    Added auto-suggest feature

--- src/components/AddGrocery/index.tsx
+++ src/components/AddGrocery/index.tsx
@@ -1,13 +1,22 @@
 import { useState, SyntheticEvent, ChangeEvent } from "react";
+import suggestList from "../../groceries";
 import styles from "./AddGrocery.module.scss";
 
 const AddGrocery = ({ addItem }: AddGroceryProps) => {
   const [newItem, setNewItem] = useState("");
-  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [hasFocus, setHasFocus] = useState(
+    document.activeElement === document.querySelector("#add-grocery-input")
+  );
 
-  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const _input = e.target.value;
     setNewItem(_input);
+  };
+
+  const handleSuggestAdd = (value: string) => {
+    addItem(value);
+    setNewItem("");
+    (document.querySelector("#add-grocery-input") as HTMLInputElement).focus();
   };
 
   const handleSubmit = (e: SyntheticEvent) => {
@@ -17,17 +26,36 @@ const AddGrocery = ({ addItem }: AddGroceryProps) => {
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <input
-        autoComplete="off"
-        className={styles.input}
-        id="add-grocery-input"
-        onChange={handleChange}
-        placeholder="Start typing; enter to add ..."
-        type="text"
-        value={newItem}
-      />
-    </form>
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          autoComplete="off"
+          className={styles.input}
+          id="add-grocery-input"
+          onBlur={() => setHasFocus(false)}
+          onFocus={() => setHasFocus(true)}
+          onChange={handleChange}
+          placeholder="Start typing; enter to add ..."
+          type="text"
+          value={newItem}
+        />
+      </form>
+      {hasFocus && newItem.length > 1 && (
+        <div className={styles["suggest-list"]}>
+          <ul>
+            {suggestList
+              .filter((item) =>
+                item.split(" ").some((item) => item.startsWith(newItem))
+              )
+              .map((item) => (
+                <li key={item} onMouseDown={() => handleSuggestAdd(item)}>
+                  {item}
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
